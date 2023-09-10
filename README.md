8fd6715f7d518ef197600938ac3de560f90dfa38
Jose-Canel-Cargo <jcanellas@hawkmail.hccfl.edu>
2023-09-10 13:41:30 -0400

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   bin/App.class 	new file:   src/App.java
    No changes made

---
+++ README.md
@@ -0,0 +1,7 @@
+Exercise 1.1 for Java
+
+This simple program prints out 3 lines of text:
+
+1. "Welcome to Java"
+2. "Welcome to Computer Science"
+3. "Programming is fun"
---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nWelcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun\n");
+    }
+}
